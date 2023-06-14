4a093301294f24f06cd77bb1fcbaa467e1a3b82b
Luis Eduardo Peraza Salazar <le.perazasalazar@gmail.com>
2023-06-14 17:25:58 +0200

    correccion de import pyodbc

--- Example_Call_StoredProcedure.py
+++ Example_Call_StoredProcedure.py
@@ -2,11 +2,12 @@ import BDD_ODBC_Connection as bddconn
 import BDD_Execute_StoredProcedure as spr
 
 # Setting the values to excecute
+odbc = 'DB_LEPS_DES' #'DB_FPY'
 Nro_serie = 32229781
-sp = "{call sp_Get_DataReport_ModHEM_Gen2_Beckhoff_By_NroSerie(" + str(Nro_serie) + ")}"
-
+sp = 'call sp_Get_AllProtocols({x1},{x2})'.format(x1='CASTELLANO', x2='FUTURE')
+print(sp)
 # Opening connection
-[conn, cursor, message] = bddconn.open_connection('DB_FPY')
+[conn, cursor, message] = bddconn.open_connection(odbc)
 
 # Excecuting the Store Procedure (sp)
 [result, message] = spr.excecute_sp(cursor, sp)
@@ -16,8 +17,8 @@ print('MENSAJE -> ' + message)
 
 # Colecting the return values of Stored Procedure
 rows1 = result.fetchall()
-cursor.nextset()
-rows2 = result.fetchall()
+# cursor.nextset()
+# rows2 = result.fetchall()
 
 # Closing the connection
 bddconn.close_connection(conn, cursor)
@@ -25,7 +26,7 @@ bddconn.close_connection(conn, cursor)
 for row in rows1:
    print('PRIMERO -- ' + str(row))
 
-# rows = cursor.fetchall()
-for row in rows2:
-   print('SEGUNDO -- ' + str(row))
+# # rows = cursor.fetchall()
+# for row in rows2:
+#    print('SEGUNDO -- ' + str(row))
 
